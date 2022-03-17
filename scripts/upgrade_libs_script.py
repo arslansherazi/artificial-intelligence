900ff28b32ff531c324086e38cb81097708cc152
arslansherazi <arslanhaider.dev@hotmail.com>
2022-03-17 14:38:17 +0500

    upgrade requirements

---
+++ scripts/upgrade_libs_script.py
@@ -0,0 +1,13 @@
+"""
+This script will remove versions of libraries in requirements.txt and then install latest versions of libraries and
+finally freeze latest versions again in requiremnets.txt
+"""
+import os
+
+req_file = open('../requirements.txt', 'rb')
+updated_req_file = open('../updated_requirements.txt', 'w')
+for library in req_file:
+    lib = '{updated_lib}\n'.format(updated_lib=library.decode().split('==')[0])
+    updated_req_file.write(lib)
+req_file.close()
+updated_req_file.close()
