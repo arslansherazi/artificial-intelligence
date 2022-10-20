c680083a3174b77ac41cc25d42b9cf81ffac82d8
Arslan <arslanhaider.dev@hotmail.com>
2022-10-20 13:33:47 +0500

    install requirements

--- scripts/upgrade_libs_script.py
+++ scripts/upgrade_libs_script.py
@@ -2,8 +2,6 @@
 This script will remove versions of libraries in requirements.txt and then install latest versions of libraries and
 finally freeze latest versions again in requirements.txt
 """
-import os
-
 req_file = open('../requirements.txt', 'rb')
 updated_req_file = open('../updated_requirements.txt', 'w')
 for library in req_file:
